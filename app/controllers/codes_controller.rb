bb54a612c3c729e8a364ef684ae229db2ea7d46c
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-28 18:45:24 +0200

    minor change in codes controller

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -4,7 +4,7 @@ skip_before_action :authenticate_user!, only: [:show]
   def index
     @codes = policy_scope(Code)
   end
-    
+
   def show
     set_code
     @item = Item.find_by(code: @code)
@@ -16,6 +16,6 @@ skip_before_action :authenticate_user!, only: [:show]
   private
 
   def set_code
-    @code = Code.find(params[:access_token])
+    @code = Code.find_by(access_token: params[:access_token])
   end
 end
