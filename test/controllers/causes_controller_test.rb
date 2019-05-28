8dd4ac2151c26829f388ef79ba988bd054c60909
Christian Fox <christian.fox01@gmail.com>
2019-05-28 18:57:10 +0200

    added root page

--- test/controllers/causes_controller_test.rb
+++ test/controllers/causes_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CausesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get causes_new_url
+    assert_response :success
+  end
+
 end
