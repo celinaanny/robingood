25001cda4628e2adb0f448ba63e984a9eba929d9
Mia Szarvas <mia.szarvas@gmail.com>
2019-05-29 11:51:32 +0200

    redirect to items page when qr codes are used up

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,18 +9,18 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.user = current_user
-
-    unless code_id == current_user.codes.last.id + 1
-
-      @item.code_id = code_id
-      authorize @item
-
-    end
-
-    if @item.save
+    authorize @item
+    if code_id == current_user.codes.last.id + 1
       redirect_to items_path
     else
-      render :new
+
+      @item.code_id = code_id
+
+      if @item.save
+        redirect_to items_path
+      else
+        render :new
+      end
     end
   end
 
