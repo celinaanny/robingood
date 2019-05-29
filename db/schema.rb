aba58de1605a07a77dccdaafcacd88d5bcd0b283
Celina Gerecke <celina.gerecke@gmail.com>
2019-05-29 19:08:59 +0200

    add map geocoder gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_123311) do
+ActiveRecord::Schema.define(version: 2019_05_29_142100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_05_29_123311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "home", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["cause_id"], name: "index_findings_on_cause_id"
     t.index ["item_id"], name: "index_findings_on_item_id"
   end
