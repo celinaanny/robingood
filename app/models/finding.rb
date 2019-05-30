96e789f562808e2988260e1040a6822ad27563db
Celina Gerecke <celina.gerecke@gmail.com>
2019-05-30 12:40:23 +0200

    geocoder implementation

--- app/models/finding.rb
+++ app/models/finding.rb
@@ -1,6 +1,7 @@
 class Finding < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :cause
   belongs_to :item
   validates :address, presence: true
 end
-
