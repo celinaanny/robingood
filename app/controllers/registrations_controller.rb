b0bd6d76370e8190c8d38bc6335a3094241b82fe
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-30 14:19:38 +0200

    update user flow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    user_codes_path # Or :prefix_to_your_route
+    registration_send_codes_path # Or :prefix_to_your_route
   end
 end
--- app/models/code.rb
+++ app/models/code.rb
@@ -4,7 +4,7 @@ class Code < ApplicationRecord
   has_one :item
 
   def url
-    "http://www.getrobingood.com/codes/#{access_token}"
+    "https://robingood.herokuapp.com/codes/#{access_token}"
   end
 
   def qrcode
