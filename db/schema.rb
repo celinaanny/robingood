04029d098b0351830075fd2e842ea95b04e73a6d
Celina Gerecke <celina.gerecke@gmail.com>
2019-05-30 12:44:43 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -52,9 +52,11 @@ ActiveRecord::Schema.define(version: 2019_05_29_162139) do
     t.string "message"
     t.bigint "cause_id"
     t.bigint "item_id"
+    t.integer "amount_cents_cents", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "amount_cents_cents"
+    t.float "latitude"
+    t.float "longitude"
     t.jsonb "payment"
     t.string "state", default: "pending"
     t.index ["cause_id"], name: "index_findings_on_cause_id"
