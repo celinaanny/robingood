1045fdca66bccbbf71c4d9f68694db67ff2b81aa
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-31 12:45:10 +0200

    updated qr codes orders

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
 import {addAttributesToQRCode} from "./qrcode";
-import {sendCongrats} from "./congrats_popup";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initSweetalert } from './sweetalert';
+import { sendCongrats } from "./congrats_popup";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
 
@@ -12,25 +12,30 @@ addAttributesToQRCode();
 initAutocomplete();
 initMapbox();
 
-initSweetalert('#sweet-alert-init', {
-  title: "Congrats... You're almost done!",
-  text: `Simply take sticker no. ${document.querySelector("#hidden-qr-count").innerHTML.trim()} from your sticker set and stick it onto your item. Done?`,
-  icon: "success"
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#create-item-btn');
-    link.click();
-  }
-});
+if (document.querySelector("#address-send-congrats")) {
+  sendCongrats('#address-send-congrats', {
+    title: "Congrats... Now all you need to do is wait!",
+    text: "Your QR codes will be in your mailbox shortly",
+    icon: "success"
+  }, (value) => {
+    if (value) {
+      const link = document.querySelector('#hidden-address-btn');
+      link.click();
+    }
+  });
+}
 
-sendCongrats('#address-send-congrats', {
-  title: "Congrats... Now all you need to do is wait!",
-  text: "Your QR codes will be in your mailbox shortly",
-  icon: "success"
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#hidden-address-btn');
-    link.click();
-  }
-});
+if (document.querySelector("#hidden-qr-count")) {
+  initSweetalert('#sweet-alert-init', {
+    title: "Congrats... You're almost done!",
+    text: `Simply take sticker no. ${ document.querySelector("#hidden-qr-count").innerHTML.trim() } from your sticker set and stick it onto your item. Done?`,
+    icon: "success"
+  }, (value) => {
+    if (value) {
+      const link = document.querySelector('#create-item-btn');
+      link.click();
+    }
+  });
+}
+
 
--- app/javascript/packs/sweetalert.js
+++ app/javascript/packs/sweetalert.js
@@ -9,4 +9,6 @@ const initSweetalert = (selector, options = {}, callback = () => {}) => {
   }
 };
 
+
 export { initSweetalert };
+
