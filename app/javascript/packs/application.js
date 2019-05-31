3c90941ca2bcf55e4507d8dbd281a5b10229fcaa
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-31 11:36:44 +0200

    update QR page and sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import {addAttributesToQRCode} from "./qrcode";
+import {sendCongrats} from "./congrats_popup";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
@@ -9,8 +10,8 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 
 addAttributesToQRCode();
 initAutocomplete();
-
 initMapbox();
+
 initSweetalert('#sweet-alert-init', {
   title: "Congrats... You're almost done!",
   text: `Simply take sticker no. ${document.querySelector("#hidden-qr-count").innerHTML.trim()} from your set of stickers and put it on your device. Done?`,
@@ -18,6 +19,17 @@ initSweetalert('#sweet-alert-init', {
 }, (value) => {
   if (value) {
     const link = document.querySelector('#create-item-btn');
+    link.click();
+  }
+});
+
+sendCongrats('#address-send-congrats', {
+  title: "Congrats... Now all you need to do is wait!",
+  text: "Your QR codes will be in your mailbox shortly",
+  icon: "success"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#hidden-address-btn');
     link.click();
   }
 });
---
+++ app/javascript/packs/congrats_popup.js
@@ -0,0 +1,12 @@
+import swal from 'sweetalert';
+
+const sendCongrats = (selector, options = {}, callback = () => {}) => {
+  const swalButton = document.querySelector(selector);
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options).then(callback); // <-- add the `.then(callback)`
+    });
+  }
+};
+
+export { sendCongrats };
