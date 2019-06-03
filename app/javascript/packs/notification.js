2d94962a2ce5fa90969eb4039d16f17848568cfd
Mia Szarvas <mia.szarvas@gmail.com>
2019-06-03 10:30:44 +0200

    fix notification badge

--- app/javascript/packs/notification.js
+++ app/javascript/packs/notification.js
@@ -1,20 +1,8 @@
 const bullseye = document.querySelector('.fa-bullseye');
-const new_finding = document.querySelector('.form-finding');
+const bell = document.querySelector('.fa-bell');
 
-const items = [ bullseye, new_finding];
-
-items.forEach((item) => {
-  if (item !== null) {
-    if (item === new_finding) {
-      item.addEventListener("submit", (event) => {
-      event.preventDefault();
-      bullseye.classList.remove("hidden");
-      });
-    };
-    if (item === bullseye) {
-      item.addEventListener("click", (event) => {
-        bullseye.classList.add("hidden");
-      });
-    };
-  }
+bell.addEventListener("click", (event) => {
+    bullseye.classList.add("hidden");
 });
+
+
