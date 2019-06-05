55b6144dd891df5c76a6b071c3e8f79e359f9b80
Celina Gerecke <celina.gerecke@gmail.com>
2019-06-05 10:53:41 +0200

    remove category form items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_083958) do
+ActiveRecord::Schema.define(version: 2019_06_05_085224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2019_06_05_083958) do
     t.datetime "updated_at", null: false
     t.boolean "disabled", default: false
     t.integer "qr_number"
-    t.string "category"
     t.index ["code_id"], name: "index_items_on_code_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
