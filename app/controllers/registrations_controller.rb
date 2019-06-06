304bacf1d3451a21f84fd3ef024ea2a321696abb
Mia Szarvas <mia.szarvas@gmail.com>
2019-06-06 11:13:21 +0200

    adding redirect to devise update method and add margin to edit form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def after_update_path_for(resource)
+    items_path
+  end
+
   def after_sign_up_path_for(resource)
     registration_send_codes_path # Or :prefix_to_your_route
   end
